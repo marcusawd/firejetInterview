02dd03d8313837da5b7a22970a48e133dc782aa8
marcusawd <angweidemarcus@gmail.com>
2023-11-08 01:55:35 +0800

    feat: Solution

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import traverse, { Node, NodePath } from "@babel/traverse";
 async function main() {
 	try {
 		const fileData = fs.readFileSync("./stringA.ts", "utf8");
+		let lintedData = fileData;
 		const ast = parse(fileData, {
 			sourceType: "module",
 			plugins: ["jsx", "typescript"],
@@ -15,35 +16,31 @@ async function main() {
 		traverse.default(ast, {
 			enter(path: NodePath) {
 				if (path.node.type === "TemplateLiteral" && path.node.leadingComments) {
-					path.node.leadingComments.some((node) => {
+					path.node.leadingComments.some(async (node) => {
 						if (node.type === "CommentBlock" && node.value === "tsx") {
 							// console.log(path.node.leadingComments);
-							tsxComments.push(path.node);
+							// tsxComments.push(path.node);
+							const codeToLint = extract(path.node, fileData);
+							const linted = await lint(codeToLint);
+							lintedData = lintedData.replace(codeToLint, linted);
+							console.log(lintedData);
+							fs.writeFileSync("answer.ts", lintedData);
 						}
 					});
 				}
 			},
 		});
-		const extracted = extract(tsxComments, fileData);
-		for (const codeToLint of extracted) {
-			const linted = await lint(codeToLint);
-			console.log(linted);
-		}
-		// console.log("File contents:", extracted);
 	} catch (err) {
 		console.error("Error reading file:", err);
 	}
 }
 
-function extract(array: Node[], code: string) {
-	const extractedCodes: string[] = [];
-	array.forEach((node) => {
-		const start = node.start!;
-		const end = node.end!;
-		const extractedCode = code.slice(start + 1, end - 1);
-		extractedCodes.push(extractedCode);
-	});
-	return extractedCodes;
+function extract(node: Node, code: string) {
+	const start = node.start!;
+	const end = node.end!;
+	const extractedCode = code.slice(start + 1, end - 1);
+
+	return extractedCode;
 }
 
 async function lint(code: string): Promise<string> {
