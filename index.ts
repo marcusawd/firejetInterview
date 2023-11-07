de464bd7dd388ad271df6680418b10d762e710f9
marcusawd <angweidemarcus@gmail.com>
2023-11-08 01:04:04 +0800

    feat: step 3

--- index.ts
+++ index.ts
@@ -1,18 +1,49 @@
 import fs from "fs";
 import { parse } from "@babel/parser";
 import prettier from "prettier";
+import traverse, { Node, NodePath } from "@babel/traverse";
 
-function main() {
+async function main() {
 	try {
 		const fileData = fs.readFileSync("./stringA.ts", "utf8");
 		const ast = parse(fileData, {
 			sourceType: "module",
 			plugins: ["jsx", "typescript"],
 		});
-		console.log("File contents:", JSON.stringify(ast));
+
+		const tsxComments: Node[] = [];
+		traverse.default(ast, {
+			enter(path: NodePath) {
+				if (path.node.type === "TemplateLiteral" && path.node.leadingComments) {
+					path.node.leadingComments.some((node) => {
+						if (node.type === "CommentBlock" && node.value === "tsx") {
+							// console.log(path.node.leadingComments);
+							tsxComments.push(path.node);
+						}
+					});
+				}
+			},
+		});
+		const extracted = extract(tsxComments, fileData);
+		for (const codeToLint of extracted) {
+			const linted = await lint(codeToLint);
+			console.log(linted);
+		}
+		// console.log("File contents:", extracted);
 	} catch (err) {
 		console.error("Error reading file:", err);
 	}
+}
+
+function extract(array: Node[], code: string) {
+	const extractedCodes: string[] = [];
+	array.forEach((node) => {
+		const start = node.start!;
+		const end = node.end!;
+		const extractedCode = code.slice(start + 1, end - 1);
+		extractedCodes.push(extractedCode);
+	});
+	return extractedCodes;
 }
 
 async function lint(code: string): Promise<string> {
